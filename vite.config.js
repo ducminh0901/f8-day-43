ad0fbf123e4eddd1ecdd753f4ef4e41b433785cc
DucMinh <minhndf8@fullstack.edu.vn>
2025-12-11 19:15:58 +0700

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/f8-day-43/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
